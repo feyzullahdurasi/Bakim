bcd4ca9a4669de05c2b3e2d9dd85f92da15b4289
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-09-26 10:53:27 +0300

    Initial Commit

---
+++ Bakim/BakimApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakimApp.swift
+//  Bakim
+//
+//  Created by Feyzullah Durası on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BakimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BakimTests/BakimTests.swift
@@ -0,0 +1,17 @@
+//
+//  BakimTests.swift
+//  BakimTests
+//
+//  Created by Feyzullah Durası on 26.09.2024.
+//
+
+import Testing
+@testable import Bakim
+
+struct BakimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
